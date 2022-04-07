31a5c3d95f31f0dbff606a24c941b99cb215bc98
Serial-Comma <alliwantisana1@gmail.com>
2022-04-07 09:14:44 +0800

    completed practical 14

--- School/Jupyter/myhello.py
+++ School/Jupyter/myhello.py
@@ -1,6 +1,29 @@
+'''
+My first module in Python.
+It contains a few classic hello-world functions.
+'''
+
 def hello_once(name):
-    print('Hello {}'.format(name))
+    '''Say hello once
+    
+    Args: 
+        A string containing the name
+    '''
+    print('Hello again {}'.format(name))
 
 def hello_many(items):
+    '''Say hello multiple times
+
+    Args: 
+        A list of names
+    '''
     for item in items:
         hello_once(item)
+
+import sys
+if __name__ == '__main__':
+    print("Script name: {}".format(sys.argv[0]))
+    if len(sys.argv) == 2:
+        hello_once(sys.argv[1])
+    if len(sys.argv) > 2:
+        hello_many(sys.argv[1:])
